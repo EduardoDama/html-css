744be9507f79e2967af94e1838d5edc4d8eefd0c
EduardoDama <102323022+EduardoDama@users.noreply.github.com>
2023-01-10 13:47:26 -0300

    Atualizei

--- login/animacao.js
+++ login/animacao.js
@@ -1,5 +1,13 @@
 function inicio(){
-    let welcome = document.getElementById('bemVindo').remove()
+    document.getElementById('bemVindo').remove()
+
+    let entrada1 = document.createElement('P').innerHTML = 'NAdaaaaaaaaaaa'
+
+    
+
+    let local = document.getElementById('escolha')
+
+    local.appendChild(entrada1)
 
     
 }
